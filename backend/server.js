5b516239f85c073476757b0b8caa39bac2f47c28
Lotan <lotan92@gmail.com>
2022-05-25 18:21:42 +0300

    *adding db.service connecting mongodb *adding 'get' and 'post' for saving and getting rates data *testing with postman

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,33 @@
 const express = require('express')
+const dbService = require('./services/db.service')
 
 const app = express();
 app.use(express.json())
 
+app.get('/api/:currency/:date', async (req,res)=>{
+    try{
+        const date = req.params.date;
+        const currency = req.params.currency;
+        const collection = await dbService.getCollection(currency)
+        const ratesData = await collection.findOne({ date: date })
+        res.send(ratesData)
+    }catch(err){
+        res.status(500).send({ err: 'Failed to get date sdf' });
+    }
+})
+
+app.post('/api/:currency', async(req,res)=>{
+    try{
+        const currency = req.params.currency;
+        let ratesData = req.body;
+        const collection = await dbService.getCollection(currency)
+        await collection.insertOne(ratesData)
+        res.send(ratesData) 
+    }catch(err){
+        res.status(500).send({ err: 'Failed to add date' });
+    }
+    
+})
 
 const port = process.env.PORT || 3030;
 
--- src/services/rate.service.js
+++ src/services/rate.service.js
@@ -7,6 +7,7 @@ export const rateService = {
 async function getRate(currency = 'EUR', startDate = '2022-05-01', endDate = '2022-05-07'){
     try{
         let res = await axios.get(`https://free.currconv.com/api/v7/convert?q=USD_${currency}&compact=ultra&date=${startDate}&endDate=${endDate}&apiKey=91dc4f4f8ba062d9f51b`)
+        console.log(res); 
         return res.data
     } catch(err){
         console.log(err);
