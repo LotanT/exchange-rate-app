c22cf98b9c047c2e65e3f1ed141d07677ef3a49a
Lotan <lotan92@gmail.com>
2022-05-25 11:26:56 +0300

    rate service

---
+++ src/services/rate.service.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export const rateService = {
+    getRate
+}
+
+async function getRate(currency = 'EUR', startDate = '2022-05-01', endDate = '2022-05-07'){
+    try{
+        let res = await axios.get(`https://free.currconv.com/api/v7/convert?q=USD_${currency}&compact=ultra&date=${startDate}&endDate=${endDate}&apiKey=91dc4f4f8ba062d9f51b`)
+        return res.data
+    } catch(err){
+        console.log(err);
+    }
+}
